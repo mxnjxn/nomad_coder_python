da89bc4030ae1eca0e2ce86ef0e5d214bbc99b08
mxnjxn <choe86898@gmail.com>
2022-02-22 01:01:53 +0900

    commit

---
+++ nomad_coder_python/data_types.py
@@ -0,0 +1,22 @@
+print('Hello World!')
+
+a = 2
+b = 3
+print(a+b)
+
+# string
+
+c = 'Hello,world'
+d = "Hello, world"
+
+# boolean - capitalize in the first letter
+c = False
+d = True  # 1
+
+# float
+a_float = 3.12
+print(a_float)
+
+# None
+a_none = None  # emptiness/null
+print(type(a_none))  # type() method
---
+++ nomad_coder_python/list.py
@@ -0,0 +1,6 @@
+# python have something called a "sequenced type"(something like a list)
+
+# Strings, list, range, tuples, *byte sequence & byte array.
+
+days = "Mon, Tue, Wed, Thur, Fri"
+print(days)
