9025991589c931f5852ecea42db407a48d0d3bac
mxnjxn <choe86898@gmail.com>
2022-02-22 01:20:45 +0900

    Hello, do work properly?

--- nomad_coder_python/data_types.py
+++ nomad_coder_python/data_types.py
@@ -2,6 +2,7 @@ print('Hello World!')
 
 a = 2
 b = 3
+
 print(a+b)
 
 # string
--- nomad_coder_python/list.py
+++ nomad_coder_python/list.py
@@ -1,6 +1,10 @@
 # python have something called a "sequenced type"(something like a list)
 
 # Strings, list, range, tuples, *byte sequence & byte array.
-
 days = "Mon, Tue, Wed, Thur, Fri"
 print(days)
+
+i = 0
+while i in range(1, 10):
+    print(i)
+    i += 1
